a4f26656cb1fdfd77bfb9100fde654953f94ed64
윤지수 <jisoo.yoon@navercorp.com>
2018-10-11 20:11:05 +0900

    Base Repository Interface 구현

---
+++ src/main/java/ac/dankook/postboard/repository/BaseRepository.java
@@ -0,0 +1,15 @@
+package ac.dankook.postboard.repository;
+
+import java.util.List;
+
+/**
+ * @param <T>  DB Domain에 맞는 Data Object
+ * @param <PK> Data Object의 Priamry Key
+ */
+public interface BaseRepository<T, PK> {
+    T insert(T entity);
+    T selectOne(PK pk);
+    List<T> selectList(Object criteria);
+    int update(T entity);
+    int delete(PK entity);
+}
